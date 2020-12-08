c0649a628a7be63be97953346ffe0ea956a4e76f
HX35EU <HX35EU>
2020-12-08 10:19:47 +0100

    asd

--- DOMParseHX35EU/src/hu/domparse/hx35eu/DOMModifyHx35eu.java
+++ DOMParseHX35EU/src/hu/domparse/hx35eu/DOMModifyHx35eu.java
@@ -21,29 +21,37 @@ public class DOMModifyHx35eu {
 
 	public static void main(String[] args)
 			throws ParserConfigurationException, IOException, SAXException, TransformerException {
+			//File objektum létrehozás
 		File file = new File("XMLHx35eu.xml");
 
+		//File beolvasás
 		Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(file);
 
+		//XML normalizálása
 		Element root = doc.getDocumentElement();
 		root.normalize();
 
+		//Az összes node lekérdezése, mely a Termék nevet viseli
 		NodeList nodes = doc.getElementsByTagName("Termék");
 
+		//Végigiterálunk a nodeokon
 		for (int i = 0; i < nodes.getLength(); i++) {
 			Node node = nodes.item(i);
 
+			//Ha a 3. nodehoz értünk
 			if (Integer.parseInt(node.getAttributes().getNamedItem("TERMEK_ID").getNodeValue()) == 3) {
 				if (node.getNodeType() == Node.ELEMENT_NODE) {
 					Element eElement = (Element) node;
+					//Átállítjuk a készletet 12000-re.
 					eElement.getElementsByTagName("Készlet").item(0).setTextContent("12000");
 				}
 			}
 		}
 
+		//File kiíratás az eredeti felülírásával
 		Transformer transformer = TransformerFactory.newInstance().newTransformer();
 		DOMSource source = new DOMSource(doc);
 		StreamResult result = new StreamResult(file);
 		transformer.transform(source, result);
 	}
-} 
+}
--- DOMParseHX35EU/src/hu/domparse/hx35eu/DOMReadHx35eu.java
+++ DOMParseHX35EU/src/hu/domparse/hx35eu/DOMReadHx35eu.java
@@ -15,26 +15,37 @@ import org.xml.sax.SAXException;
 public class DOMReadHx35eu {
 
 	public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException {
+		
+		//File objektum létrehozás
 		File file = new File("XMLHx35eu.xml");
 
+		//File beolvasás
 		Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(file);
 		Element root = doc.getDocumentElement();
+		
+				//XML normalizálása
 		root.normalize();
 
+		//Kiíró fgv meghívása
 		printXML(root, "");
 	}
 
 	public static void printXML(Node root, String indentation) {
+	//kiválasztja a root nodeod
 		String nodename = root.getNodeName();
 
+		//Ha a node szöveges, akkor kiírja
 		if (!nodename.contains("text")) {
 			System.out.println(indentation + nodename + printAttrs(root));
 		}
 
+		//Minél mélyebben vagyunk a fában, annál beljebb tesszük a nodeokat a consoleban
 		indentation += "\t";
 
+		//Az összes childnode lekérése
 		NodeList childNodes = root.getChildNodes();
 
+		//Az összes childnodera megy a ciklus, ha komplex, akkor ismét meghívjuk rá a fgv-t, mert több node lehet még benne, ha pedig nem akkor simán kiírjuk
 		for (int i = 0; i < childNodes.getLength(); i++) {
 			Node child = childNodes.item(i);
 			boolean complexType = child.getTextContent().contains("\n");
@@ -48,9 +59,12 @@ public class DOMReadHx35eu {
 		}
 	}
 
+	//Az attribútumok kiírására szolgáló fgv.
 	public static String printAttrs(Node child) {
+	// A lehetséges attribútumok listája
 		String[] attrset = { "VEVO_ID", "KOSAR_ID", "TIPUS_ID", "TERMEK_ID" };
 		String attrs = "";
+		//Ha a vizsgált node tartalmaz ilyen attribútumot, akkor összefűzi
 		for (String attr : attrset) {
 			if (child.getAttributes().getNamedItem(attr) != null)
 				attrs += " " + child.getAttributes().getNamedItem(attr);
@@ -59,4 +73,4 @@ public class DOMReadHx35eu {
 		return attrs;
 	}
 
-} 
+}
